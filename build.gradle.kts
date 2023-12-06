1323ee8e03fcc70edef92ba6fe5fd4b0da7ef4dd
dokar3 <68095777+dokar3@users.noreply.github.com>
2023-12-06 17:49:16 +0800

    Prevent the ktLineCounter task from getting executed on gradle sync

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,26 @@
 subprojects {
     tasks.register("ktLineCounter") {
-        var lineCount = 0
-        var nonEmptyLineCount = 0
-        for (ktFile in File(project.projectDir, "src/main").walk()) {
-            if (ktFile.isDirectory || !ktFile.name.endsWith(".kt")) {
-                continue
-            }
-            ktFile.forEachLine {
-                lineCount++
-                if (it.isNotBlank()) {
-                    nonEmptyLineCount++
+        doLast {
+            var lineCount = 0
+            var nonEmptyLineCount = 0
+            for (ktFile in File(project.projectDir, "src/main").walk()) {
+                if (ktFile.isDirectory || !ktFile.name.endsWith(".kt")) {
+                    continue
+                }
+                ktFile.forEachLine {
+                    lineCount++
+                    if (it.isNotBlank()) {
+                        nonEmptyLineCount++
+                    }
                 }
             }
-        }
-        println(
-            """
+            println(
+                """
                 ======= .kt Line Counter =======
                           Lines: $lineCount
                 Non-empty lines: $nonEmptyLineCount
             """.trimIndent()
-        )
+            )
+        }
     }
 }
